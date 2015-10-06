8e59a826663e928564bc9e2330ce42b3417ea142
rogermelich <rogermelich@iesebre.com>
2015-10-06 20:36:25 +0200

    Add namespace and directory src

---
+++ src/hello.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Com\Iesebre\Dam2\rogermelich\helloworldComposer;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+
+/**
+ * Created by PhpStorm.
+ * User: roger
+ * Date: 6/10/15
+ * Time: 19:31
+ */
+class hello
+{
+    /**
+     * hello constructor.
+     */
+    public function __construct()
+    {
+        $this->faker = \Faker\Factory::create();
+    }
+
+
+    public function sayHello()
+    {
+        echo "Hola " . $this->faker->name . "!\n";
+ }
+}
